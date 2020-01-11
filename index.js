678bffc2daa8fa0777639e1a2eae58a75e3d54a9
Isiah Lloyd <robotdude101@gmail.com>
2020-01-11 14:25:58 -0500

    Prepare for public release!

--- index.js
+++ index.js
@@ -1,27 +1,68 @@
-require('dotenv').config()
-
-const MailChimp = require('mailchimp-api-v3')
 const request = require('request-promise')
-const emoji = require('node-emoji')
 
 module.exports.run = (event, context, callback) => {
-    const mailchimpListId = process.env.MAILCHIMP_LIST_ID
-    const mailchimpApiKey = process.env.MAILCHIMP_API_KEY
-    const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL
-
-    const mailchimp = new MailChimp(mailchimpApiKey)
-
-    mailchimp.get(`/lists/${mailchimpListId}`, { fields: 'stats' })
-        .then(result => {
-            // get the current member count from the MailChimp list
-            const stats = result.stats
-            const memberCount = stats.member_count
-            return memberCount
-        })
-        .then(memberCount => {
+    const revucApiKey = process.env.REVUC_API_KEY;
+    const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+    const RANDOM_STATS_OPTIONS = ['gender', 'schools', 'majors', 'ethnicities', 'shirtSizes', 'educationLevels', 'allergens'];
+    const requestOptions = {
+        uri: 'https://revolutionuc-api.herokuapp.com/api/admin/stats',
+        headers: {
+            'Content-Type': 'Application/json',
+            'X-API-KEY': revucApiKey
+        },
+        json: true
+    }
+    request(requestOptions)
+        .then(stats => {
+            const totalRegs = stats.numRegistrants;
+            const last24hrs = stats.last24hrs;
             // prepare the slack message and hit the slack webhook url
-            const randomEmoji = [1,2,3].map(() => `:${emoji.random().key}:`).join(' ')
-            const slackMessage = `Mailing list subscriber count as of today: *${memberCount}* ${randomEmoji}`
+            let slackMessage = `Good evening organizers! We currently have *${totalRegs}* (:arrow_up: ${last24hrs} in the past 24 hours) students registered for RevolutionUC!
+                                  \n:chart_with_upwards_trend: Now for todays random stat about our participants!\n`
+            switch (RANDOM_STATS_OPTIONS[Math.floor(Math.random() * RANDOM_STATS_OPTIONS.length)]) {
+                case 'gender':
+                    slackMessage += `:female-technologist: Here is the breakdown of our participants genders :male-technologist:`
+                    stats.gender.forEach((el) => {
+                        slackMessage += `\n• ${el.gender} [${el.count}]`;
+                    });
+                break;
+                case 'schools':
+                    slackMessage += `:school: Here are the top 5 schools registered for RevolutionUC!`
+                    stats.top5schools.forEach((el) => {
+                        slackMessage += `\n• ${el.school} [${el.count}]`;
+                    });
+                break;
+                case 'majors':
+                    slackMessage += `Here are the top 5 majors of participants registered for RevolutionUC:`
+                    stats.top5majors.forEach((el) => {
+                        slackMessage += `\n• ${el.major} [${el.count}]`;
+                    });
+                break;
+                case 'ethnicities':
+                    slackMessage += `Here is the breakdown of our participants ethnicities:`
+                    stats.ethnicities.forEach((el) => {
+                        slackMessage += `\n• ${el.ethnicity} [${el.count}]`;
+                    });
+                break;
+                case 'shirtSizes':
+                    slackMessage += `:tshirt: Here is the breakdown of our participants shirt sizes:`
+                    stats.shirtSizes.forEach((el) => {
+                        slackMessage += `\n• ${el.shirtSize} [${el.count}]`;
+                    });
+                break;
+                case 'educationLevels':
+                    slackMessage += `Here is the breakdown of our participants education level:`
+                    stats.educationLevels.forEach((el) => {
+                        slackMessage += `\n• ${el.educationLevel} [${el.count}]`;
+                    });
+                break;
+                case 'allergens':
+                    slackMessage += `:penuts: Here is the breakdown of our participants _known_ allergens level :glass_of_milk:`
+                    stats.allergens.forEach((el) => {
+                        slackMessage += `\n• ${el.allergens} [${el.count}]`;
+                    });
+                break;
+            }
             const options = {
                 method: 'POST',
                 uri: slackWebhookUrl,
